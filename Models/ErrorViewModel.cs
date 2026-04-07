86d0bc7a8f6912b5c30141970a68dd29912e9f62
dylan-ros <dylan.rosales9@gmail.com>
2026-04-07 17:55:21 -0600

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIVS_certi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
